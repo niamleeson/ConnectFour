4af37591f6247b59d79936832584221ad2993524
Jay Kim <jaykim9388@gmail.com>
2017-01-27 17:03:53 -0500

    fixed missing template helper

--- frontend/app/controllers/index.js
+++ frontend/app/controllers/index.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  modalDialog: Ember.inject.service(),
+  gameState: Ember.inject.service(),
+  session: Ember.inject.service('session'),
+
+  actions: {
+    chooseDifficulty() {
+      this.get('modalDialog').openDialog('difficulty-dialog');
+    },
+    createNewGame() {
+      this.get('modalDialog').openDialog('confirmation-dialog')
+        .then(() => {
+          this.get('gameState').createNewGame();
+        });
+    },
+    closeDialog() {
+      this.get('modalDialog').closeDialog();
+    },
+    invalidateSession() {
+      this.get('session').invalidate();
+    }
+  }
 });
