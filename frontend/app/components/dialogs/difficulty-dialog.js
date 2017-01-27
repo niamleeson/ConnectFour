9f765b3f5b65320b69d741533fdf86c2d6ddc38b
Jay Kim <jaykim9388@gmail.com>
2017-01-27 16:53:48 -0500

    add a dialog for game difficulty selection

---
+++ frontend/app/components/dialogs/difficulty-dialog.js
@@ -0,0 +1,49 @@
+import Ember from 'ember';
+import Dialog from 'frontend/mixins/dialog';
+
+export default Ember.Component.extend(Dialog, {
+  gameState: Ember.inject.service(),
+  difficulty: Ember.computed.alias('gameState.difficulty'),
+  
+  oneColor: Ember.computed('difficulty', function () {
+    if (this.get('difficulty') === 1) {
+      return Ember.String.htmlSafe('#D32F2F');
+    }
+    return Ember.String.htmlSafe('#E0E0E0');
+  }),
+  twoColor: Ember.computed('difficulty', function () {
+    if (this.get('difficulty') === 2) {
+      return Ember.String.htmlSafe('#D32F2F');
+    }
+    return Ember.String.htmlSafe('#E0E0E0');
+  }),
+  threeColor: Ember.computed('difficulty', function () {
+    if (this.get('difficulty') === 3) {
+      return Ember.String.htmlSafe('#D32F2F');
+    }
+    return Ember.String.htmlSafe('#E0E0E0');
+  }),
+  fourColor: Ember.computed('difficulty', function () {
+    if (this.get('difficulty') === 4) {
+      return Ember.String.htmlSafe('#D32F2F');
+    }
+    return Ember.String.htmlSafe('#E0E0E0');
+  }),
+  fiveColor: Ember.computed('difficulty', function () {
+    if (this.get('difficulty') === 5) {
+      return Ember.String.htmlSafe('#D32F2F');
+    }
+    return Ember.String.htmlSafe('#E0E0E0');
+  }),
+
+  actions: {
+    difficultySelected(difficulty) {
+      this.set('gameState.difficulty', difficulty);
+      //maybe create a new game?
+      this.closeDialog();
+    },
+    cancel() {
+      this.closeDialogAndReject();
+    }
+  }
+});
