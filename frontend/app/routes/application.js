cdd8bb6fc45bce0a36dbc8f14ecfc442508f3f6e
Jay Kim <jaykim9388@gmail.com>
2017-01-29 20:21:07 -0500

    add a way to fetch current user when the application was already authenticated

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+const { service } = Ember.inject;
+
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  currentUser: service(),
+
+  beforeModel() {
+    return this.loadCurrentUser();
+  },
+
+  sessionAuthenticated() {
+    this._super(...arguments);
+    this.loadCurrentUser().catch(() => this.get('session').invalidate());
+  },
+
+  loadCurrentUser() {
+    return this.get('currentUser').load();
+  }
+});
